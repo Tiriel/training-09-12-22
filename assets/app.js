bab421613c114f9e49d79e50d4587dba20163726
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-09-13 11:08:43 +0200

    Encore

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+import $ from 'jquery';
+
+// start the Stimulus application
+// import './bootstrap';
+
+$(document).ready(function () {
+    $('h1').click(function () {
+        console.log('clicked!');
+    })
+});
+global.$ = global.jQuery = $;
